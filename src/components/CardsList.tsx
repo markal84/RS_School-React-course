2abb43d2df486f8c3edffe8536c324f12a6fad20
Mar Kal <marc@example.com>
2023-03-16 13:39:59 +0100

    feat: add loading products state

--- src/components/CardsList.tsx
+++ src/components/CardsList.tsx
@@ -5,22 +5,26 @@ import Card from './Card';
 interface IProductsState {
   products: IProduct[];
   error: boolean;
+  isLoading: boolean;
 }
 
 export default class CardsList extends Component<IProductsState> {
   state: IProductsState = {
     products: [],
     error: false,
+    isLoading: false,
   };
 
   url =
     'https://dummyjson.com/products?limit=10&select=title,description,price,rating,brand,category,thumbnail';
 
   componentDidMount() {
+    this.setState({ isLoading: true });
     fetch(this.url)
       .then((res) => res.json())
       .then((res) =>
         this.setState({
+          isLoading: false,
           products: res.products,
         })
       )
@@ -32,12 +36,13 @@ export default class CardsList extends Component<IProductsState> {
   }
 
   render() {
-    const { products, error } = this.state;
+    const { products, error, isLoading } = this.state;
 
     return (
       <div className="CardsList">
-        {!error && products.map((product) => <Card key={product.id} {...product} />)}
-        {error && <div>Someting went wrong</div>}
+        {isLoading && <div className="Center Mb-2 Mt-2">Loading products...</div>}
+        {!error && !isLoading && products.map((product) => <Card key={product.id} {...product} />)}
+        {error && <div className="Center">Someting went wrong</div>}
       </div>
     );
   }
