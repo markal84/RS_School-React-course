139d29b9f865de1a98860f5e1a3c0596049a7df4
Mar Kal <marc@example.com>
2023-03-19 18:38:59 +0100

    refactor: app styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,14 +3,17 @@ import { Link } from 'react-router-dom';
 
 const Navbar: FC = () => {
   return (
-    <ul className="Navbar">
-      <li className="Navbar-link">
-        <Link to="/">Home</Link>
-      </li>
-      <li className="Navbar-link">
-        <Link to="/about">About</Link>
-      </li>
-    </ul>
+    <nav>
+      <ul className="Navbar">
+        <li className="Navbar-link">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="Navbar-link">
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <div className="Navbar-dot"></div>
+    </nav>
   );
 };
 
